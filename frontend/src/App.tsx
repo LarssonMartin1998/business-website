93c848457c50f95f9fc0ee8ce9ca9e0a66e55953
Martin Larsson <larssonmartin1998-mac@Martins-MacBook-Pro.local>
2025-07-26 13:37:51 +0200

    Add blockout for services and experience to home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,19 @@ function App() {
           subheadline='|&nbsp; Senior Consultant'
           paragraph='I help teams build cool shit in C++, C, Zig, Rust and other wack languages.'
         />
+        <div className={twMerge(base.bg.default, 'flex', 'h-64', 'justify-center')}>
+          <ul className={twMerge('flex', 'items-center', 'gap-x-10', 'text-3xl', base.fg.default)}>
+            <li>Farm Heroes Saga</li>
+            <li>Mannequin</li>
+            <li>Cities VR</li>
+            <li>The Curious Tale of the Stolen Pets</li>
+            <li>Apex Construct</li>
+          </ul>
+        </div>
+        <div className={twMerge(base.bg.accent, 'flex', 'h-128', 'justify-center', 'p-5')}>
+          <h2 className={twMerge(base.fg.accent, 'font-bold', 'text-5xl')}>Services</h2>
+          <p className={base.bg.accent}></p>
+        </div>
       </main>
     </>
   )
