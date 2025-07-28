95bb3fda8b66b455c2cee523297677f6308edc6c
Martin Larsson <larssonmartin1998-mac@Martins-MacBook-Pro.local>
2025-07-29 00:52:54 +0200

    Change card to use className instead of w and h

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -4,8 +4,7 @@ import { base, TwColor } from '../designSystem/colors';
 
 interface CardProps {
   children: React.ReactNode;
-  w: string,
-  h: string,
+  className: string;
 }
 
 function ListCardDefault(props: CardProps) {
@@ -30,8 +29,8 @@ function CardAccent(props: CardProps) {
   return Card(base.border.accent, base.bg.accentCard, props);
 }
 
-function Card(border: TwColor, bg: TwColor, { children, w, h }: CardProps) {
-  return (<div className={twMerge(border, bg, 'border-2', 'flex', 'flex-col', 'text-center', 'justify-center', 'items-center', 'rounded-2xl', 'p-2.5', w, h)}>
+function Card(border: TwColor, bg: TwColor, { children, className }: CardProps) {
+  return (<div className={twMerge(border, bg, 'border-2', 'flex', 'flex-col', 'text-center', 'justify-center', 'items-center', 'rounded-2xl', 'p-2.5', className)}>
     {children}
   </div>);
 }
