89325dccc3b36ad613254e0b9dca291b1a610a97
Martin Larsson <larssonmartin1998-mac@Martins-MacBook-Pro.local>
2025-07-28 12:58:07 +0200

    Refactor ButtonComponent and export CustomButton

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import { twMerge } from 'tailwind-merge';
 import { base, hover, type TwColor } from './../designSystem/colors.ts';
 
-interface InternalButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
-  size?: 'sm' | 'md' | 'lg';
-  className?: string;
-  bgColor: TwColor;
-  hoverBgColor: TwColor;
-  textColor: TwColor;
+interface ButtonColor {
+  default: TwColor;
+  hover?: TwColor;
 }
 
 interface SealedButtonProps {
@@ -19,32 +15,33 @@ interface SealedButtonProps {
   type?: 'button' | 'submit' | 'reset';
 }
 
-function InternalButton({
-  children,
-  size = 'md',
-  bgColor,
-  hoverBgColor,
-  textColor,
-  className,
-  ...props }: InternalButtonProps) {
+interface CustomButtonProps {
+  border?: ButtonColor,
+  bg: ButtonColor,
+  fg: ButtonColor,
+}
+
+function CustomButton({ children, size = 'md', border, bg, fg, className, ...props }: CustomButtonProps & SealedButtonProps) {
   const base = 'inline-flex items-center justify-center rounded-md font-medium transition-colors disabled:opacity-50 disabled:pointer-events-none';
   const focus = 'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-deep-forest-blue';
 
   const sizes = {
-    sm: 'px-3 py-1.5 text-sm',
-    md: 'px-4 py-2 text-base',
-    lg: 'px-6 py-3 text-lg',
+    sm: 'px-3 py-1.5 text-sm w-24 h-12',
+    md: 'px-4 py-2 text-base w-36 h-14',
+    lg: 'px-6 py-3 text-lg w-48 h-14',
   };
 
-  console.log(className);
+  const borderProps = border != undefined ? twMerge(border.default, border.hover, 'border-2') : undefined;
 
   const buttonClassName = twMerge(
     base,
     focus,
     sizes[size],
-    bgColor,
-    hoverBgColor,
-    textColor,
+    borderProps,
+    bg.default,
+    bg.hover,
+    fg.default,
+    fg.hover,
     className
   );
 
@@ -54,26 +51,40 @@ function InternalButton({
     </button>)
 }
 
-function DefaultButton({ ...props }: SealedButtonProps) {
+function DefaultButton(props: SealedButtonProps) {
   return (
-    <InternalButton
-      bgColor={base.bg.accent}
-      hoverBgColor={hover.bg.accent}
-      textColor={base.fg.accent}
+    <CustomButton
+      bg={{
+        default: base.bg.accent,
+        hover: hover.bg.accent
+      }}
+      fg={{
+        default: base.fg.accent,
+        hover: hover.fg.accent
+      }}
       {...props}
     />
   );
 }
 
-function AlertButton({ ...props }: SealedButtonProps) {
+function AlertButton(props: SealedButtonProps) {
   return (
-    <InternalButton
-      bgColor={base.bg.alert}
-      hoverBgColor={hover.bg.alert}
-      textColor={base.fg.alert}
+    <CustomButton
+      border={{
+        default: base.border.alert,
+        hover: hover.border.alert
+      }}
+      bg={{
+        default: base.bg.alert,
+        hover: hover.bg.alert
+      }}
+      fg={{
+        default: base.fg.alert,
+        hover: hover.fg.alert
+      }}
       {...props}
     />
   );
 }
 
-export { DefaultButton, AlertButton };
+export { CustomButton, DefaultButton, AlertButton };
