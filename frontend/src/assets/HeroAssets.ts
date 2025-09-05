2c543ee54e3cd9637e6cc6b118b1a957b0a1fd28
Martin Larsson <larssonmartin1998@gmail.com>
2025-09-05 15:13:11 +0200

    Improve hero image sizes and responsiveness.

--- frontend/src/assets/HeroAssets.ts
+++ frontend/src/assets/HeroAssets.ts
@@ -1,6 +1,13 @@
 export const heroBackground = {
   sm: new URL('./images/hero_40.webp', import.meta.url).href,
-  md: new URL('./images/hero_60.webp', import.meta.url).href,
+  sm_avif: new URL('./images/hero_40.avif', import.meta.url).href,
+
+  md: new URL('./images/hero_60.avif', import.meta.url).href,
+  md_bkp: new URL('./images/hero_60.webp', import.meta.url).href,
+
   lg: new URL('./images/hero_80.webp', import.meta.url).href,
+  lg_avif: new URL('./images/hero_80.avif', import.meta.url).href,
+
   xl: new URL('./images/hero_100.webp', import.meta.url).href,
+  xl_avif: new URL('./images/hero_100.avif', import.meta.url).href,
 };
--- frontend/src/components/home/HeroBackground.tsx
+++ frontend/src/components/home/HeroBackground.tsx
@@ -1,21 +1,26 @@
-import { heroBackground } from 'assets/HeroAssets.ts';
+import { heroBackground } from 'assets/HeroAssets';
 
 function HeroBackground() {
   return (
-
     <picture>
-      <source type="image/webp" srcSet={heroBackground.xl} media='(min-width: 2150px)' />
-      <source type="image/webp" srcSet={heroBackground.lg} media='(min-width: 1280px)' />
-      <source type="image/webp" srcSet={heroBackground.md} media='(min-width: 640px)' />
+      <source type="image/avif" srcSet={heroBackground.xl_avif} media="(min-width: 2150px)" />
+      <source type="image/webp" srcSet={heroBackground.xl} media="(min-width: 2150px)" />
+
+      <source type="image/avif" srcSet={heroBackground.lg_avif} media="(min-width: 1280px)" />
+      <source type="image/webp" srcSet={heroBackground.lg} media="(min-width: 1280px)" />
+
+      <source type="image/avif" srcSet={heroBackground.md} media="(min-width: 640px)" />
+      <source type="image/webp" srcSet={heroBackground.md_bkp} media="(min-width: 640px)" />
+
+      <source type="image/avif" srcSet={heroBackground.sm_avif} />
       <img
         src={heroBackground.sm}
-        alt='Hero Background'
-        fetchPriority='high'
-        decoding='async'
-        className='absolute inset-0 w-full h-full object-cover'
+        alt="Hero Background"
+        fetchPriority="high"
+        decoding="async"
+        className="absolute inset-0 w-full h-full object-cover"
       />
     </picture>
-
   );
 }
 
