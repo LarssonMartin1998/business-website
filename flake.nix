e687063da349ba3ae2b1e710a8a6f15b8e52dc0c
Martin Larsson <larssonmartin@M3-MBP-Max.local>
2025-09-11 20:09:12 +0200

    Bump flake version to 1.0.5

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
           # Combined deployment package
           default = pkgs.stdenv.mkDerivation {
             pname = "business-website";
-            version = "1.0.4";
+            version = "1.0.5";
 
             unpackPhase = "true"; # No source to unpack
 
